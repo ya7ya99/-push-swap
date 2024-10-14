9e107451868ceaea87921bb87036e7391170b50e
ynaitedd <yobobies431@gmailcom>
2024-10-14 18:04:46 +0100

    1

--- main.go
+++ main.go
@@ -1,37 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) < 1 {
+	fmt.Println("Please enter numbers separated by newlines (Ctrl+D to finish):")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	numbers := make(map[int]bool)
+	var a []int
+
+	
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue 
+		}
+
+		numStrings := strings.Split(line, "\n")
+		for _, numStr := range numStrings {
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				fmt.Println("Error converting input to integer:", err)
+				continue
+			}
+
+			
+			if numbers[num] {
+				fmt.Println("Number already exists:", num)
+				continue
+			}
+
+			numbers[num] = true
+			a = append(a, num)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
 		return
 	}
 
-	numbers := make(map[int]bool)
-	a := []int{}
-	// stackB := []int{}
-
-	for _, arg := range args {
-		num, err := strconv.Atoi(arg)
-		if err != nil {
-			fmt.Println("Error converting argument to integer")
-			return
-		}
-
-		if numbers[num] {
-			fmt.Println("Number already exists")
-			return
-		}
-
-		numbers[num] = true
-		a = append(a, num)
-	}
-
-	fmt.Println(a)
-	// fmt.Printf("Number of unique inputs: %d\n", len(numbers))
+	fmt.Println("Unique numbers:", a)
+	fmt.Printf("Number of unique inputs: %d\n", len(numbers))
 }
